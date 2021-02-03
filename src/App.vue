7970158c6afcdeb86629009e5c9d5bb52e37428c
fjc <>
2021-02-03 22:33:05 +0800

    feat:项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -16,7 +15,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+@import '@/assets/reset.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
